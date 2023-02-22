aa5f19ee77f170b0055f1f1784d8fb50e9ed4460
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-02-22 19:27:00 +0200

    Updated TranscribeController.cs

--- Controllers/TranscribeController.cs
+++ Controllers/TranscribeController.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using AsyncKeyedLock;
 using Microsoft.AspNetCore.Mvc;
-using Pastel;
 using Serilog;
 using static WhisperAPI.Globals;
 
