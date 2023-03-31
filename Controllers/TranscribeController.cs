3e078aa1ced5b55e55964578f5dd6f9bd2555a3b
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-04-01 00:59:32 +0300

    Swiched to Multipart Form Data Request
    Now file validation is performed first

--- Controllers/TranscribeController.cs
+++ Controllers/TranscribeController.cs
@@ -1,5 +1,6 @@
 using AsyncKeyedLock;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using WhisperAPI.Models;
 using WhisperAPI.Services;
 using static WhisperAPI.Globals;
@@ -10,25 +11,44 @@ namespace WhisperAPI.Controllers;
 [Route("[controller]")]
 public sealed class Transcribe : ControllerBase
 {
+    #region Constructor
+
     private readonly AsyncKeyedLocker<string> _asyncKeyedLocker;
-
     private readonly ITranscriptionService _transcriptionService;
+    private readonly FileExtensionContentTypeProvider _provider;
 
-    public Transcribe(AsyncKeyedLocker<string> asyncKeyedLocker, ITranscriptionService transcriptionService)
+    public Transcribe(AsyncKeyedLocker<string> asyncKeyedLocker,
+        ITranscriptionService transcriptionService,
+        FileExtensionContentTypeProvider provider)
     {
         _asyncKeyedLocker = asyncKeyedLocker;
         _transcriptionService = transcriptionService;
+        _provider = provider;
     }
 
+    #endregion
+
     [HttpPost]
-    public async Task<IActionResult> Post([FromBody] PostRequest request)
+    public async Task<IActionResult> Post([FromForm] PostRequest request, [FromForm] IFormFile? file)
     {
-        if (string.IsNullOrEmpty(request.File))
+        // Return if no file is provided
+        if (file is null || file.Length is 0)
             return BadRequest(_transcriptionService.FailResponse(ErrorCodesAndMessages.NoFile,
                 ErrorCodesAndMessages.NoFileMessage));
 
+        // Get file extension
+        var fileExtension = _provider.TryGetContentType(file.FileName, out var contentType)
+            ? contentType
+            : file.ContentType;
+
+        // Return if file is not audio or video
+        if (!fileExtension.StartsWith("audio/") && !fileExtension.StartsWith("video/"))
+            return BadRequest(_transcriptionService.FailResponse(ErrorCodesAndMessages.InvalidFileType,
+                ErrorCodesAndMessages.InvalidFileTypeMessage));
+
+        // Queue the transcription request
         using var loc = await _asyncKeyedLocker.LockAsync(Key).ConfigureAwait(false);
-        var response = await _transcriptionService.HandleTranscriptionRequest(request);
+        var response = await _transcriptionService.HandleTranscriptionRequest(file, request);
         return response.Success ? Ok(response) : BadRequest(response);
     }
 }
