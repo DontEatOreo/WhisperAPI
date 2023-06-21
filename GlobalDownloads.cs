5b6b8700504df3fd441d36146931532c4710426d
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-06-21 17:02:19 +0300

    feat: update GlobalDownloads URL and switch URLs to Uri

--- GlobalDownloads.cs
+++ GlobalDownloads.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using System.IO.Compression;
-using ILogger = Serilog.ILogger;
 
 namespace WhisperAPI;
 
@@ -11,9 +10,9 @@ public class GlobalDownloads
     private readonly Globals _globals;
     private readonly ModelsClient _modelsClient;
     private readonly WhisperClient _whisperClient;
-    private readonly ILogger _logger;
+    private readonly Serilog.ILogger _logger;
 
-    public GlobalDownloads(Globals globals, ModelsClient modelsClient, ILogger logger, WhisperClient whisperClient)
+    public GlobalDownloads(Globals globals, ModelsClient modelsClient, Serilog.ILogger logger, WhisperClient whisperClient)
     {
         _globals = globals;
         _modelsClient = modelsClient;
@@ -28,18 +27,19 @@ public class GlobalDownloads
     public async Task Model(WhisperModel whisperModel)
     {
         var modelString = whisperModel.ToString().ToLower();
-        // Source: https://huggingface.co/datasets/ggerganov/whisper.cpp/tree/main
-        var modelUrl = $"https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-{modelString}.bin";
-        var modelPath = Path.Combine(_globals.WhisperFolder, $"ggml-{modelString}.bin");
+        // Source: https://huggingface.co/ggerganov/whisper.cpp/raw/main/
+        var modelName = $"ggml-{modelString}.bin";
+        Uri modelUri = new($"https://huggingface.co/ggerganov/whisper.cpp/raw/main/{modelName}");
+        var modelPath = Path.Combine(_globals.WhisperFolder, modelName);
 
         _logger.Information("Downloading {WhisperModel} model...", whisperModel);
-        await _modelsClient.Get(modelUrl, modelPath);
+        await _modelsClient.Get(modelUri, modelPath);
         _logger.Information("Downloaded {WhisperModel} model", whisperModel);
     }
 
     public async Task Whisper()
     {
-        var fileName = Path.GetFileName(_globals.WhisperUrl);
+        var fileName = Path.GetFileName(_globals.WhisperUrl.ToString());
 
         var tempPath = Path.GetTempPath();
         var zipPath = Path.Combine(tempPath, fileName);
@@ -49,23 +49,21 @@ public class GlobalDownloads
 
         _logger.Information("Downloading Whisper...");
         await _whisperClient.Get(_globals.WhisperUrl, zipPath);
-        _logger.Information("Downloaded {WhisperModel} model", fileName);
+        _logger.Information("Finish downloading: {WhisperModel} model", fileName);
 
         _logger.Information("Unzipping Whisper...");
         ZipFile.ExtractToDirectory(zipPath, Path.GetTempPath());
 
         _logger.Information("Compiling Whisper...");
-        using Process process = new()
+        ProcessStartInfo makeInfo = new()
         {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "make",
-                WorkingDirectory = unzipPath,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            }
+            FileName = "make",
+            WorkingDirectory = unzipPath,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
         };
+        using Process process = new() { StartInfo = makeInfo };
         process.Start();
         await process.WaitForExitAsync();
         _logger.Information("Finished compiling Whisper");
@@ -76,7 +74,10 @@ public class GlobalDownloads
         if (!Directory.Exists(_globals.WhisperFolder))
             Directory.CreateDirectory(_globals.WhisperFolder);
 
-        File.Move(Path.Combine(unzipPath, "main"), Path.Combine(_globals.WhisperFolder, "main"));
+        var source = Path.Combine(unzipPath, "main");
+        var dest = Path.Combine(_globals.WhisperFolder, "main");
+        File.Move(source, dest);
+
         File.Delete(zipPath);
         Directory.Delete(unzipPath, true);
     }
