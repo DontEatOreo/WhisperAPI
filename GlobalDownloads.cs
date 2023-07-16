2312b5a8e7d1e65a4842bc0273f476fa84c9ee68
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-07-16 14:43:56 +0300

    feat: add error handling in GlobalDownloads for unset Whisper folder
    - Introduced a constant for a standard error message to provide more clarity when a WHISPER folder error occurs.
    - Refactored variable naming from `ggmlType` to `type` for simplification.

--- GlobalDownloads.cs
+++ GlobalDownloads.cs
@@ -7,6 +7,8 @@ public class GlobalDownloads
 {
     private readonly WhisperSettings _whisperSettings;
     private readonly Serilog.ILogger _logger;
+    
+    private const string ErrorMessage = "Whisper folder not set in appsettings.json";
 
     public GlobalDownloads(IOptions<WhisperSettings> options, Serilog.ILogger logger)
     {
@@ -14,16 +16,24 @@ public class GlobalDownloads
         _logger = logger;
     }
 
-    public async Task DownloadModel(GgmlType ggmlType)
+    public async Task DownloadModel(GgmlType type)
     {
-        _logger.Information("Downloading {WhisperModel} model...", ggmlType.ToString());
-        await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
+        _logger.Information("Downloading {WhisperModel} model...", type.ToString());
+        await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(type);
+        
+        if (_whisperSettings.Folder is null)
+        {
+            _logger.Error(ErrorMessage);
+            throw new DirectoryNotFoundException(ErrorMessage);
+        }
+        
         var whisperFolder = Path.GetFullPath(_whisperSettings.Folder);
         if (!Directory.Exists(whisperFolder))
-            throw new DirectoryNotFoundException($"Whisper folder not found at {whisperFolder}");
-        var modelPath = Path.Combine(whisperFolder, $"ggml-{ggmlType.ToString().ToLower()}.bin");
+            throw new DirectoryNotFoundException(ErrorMessage);
+        
+        var modelPath = Path.Combine(whisperFolder, $"ggml-{type.ToString().ToLower()}.bin");
         await using var fileWriter = File.OpenWrite(modelPath);
         await modelStream.CopyToAsync(fileWriter);
-        _logger.Information("Downloaded {WhisperModel} model", ggmlType.ToString());
+        _logger.Information("Downloaded {WhisperModel} model", type.ToString());
     }
 }
