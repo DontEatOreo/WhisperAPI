adf4924b8620a5f2966d3b8ffbf5e62ca3213674
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-12-29 15:05:39 +0200

    refactor: use ILogger instead of Log

--- Globals.cs
+++ Globals.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using Serilog;
+using ILogger = Serilog.ILogger;
 
 namespace WhisperAPI;
 
@@ -8,26 +9,27 @@ public class Globals
     public readonly string WhisperFolder;
     public readonly string AudioFilesFolder;
 
+    private readonly ILogger _logger = Log.ForContext<Globals>();
+
     public Globals(IOptions<WhisperSettings> options)
     {
-        if (options.Value.Folder is null)
-        {
-            const string errorMessage = "Whisper folder not found. Please set the Whisper folder in appsettings.json";
-            throw new DirectoryNotFoundException(errorMessage);
-        }
+        if (string.IsNullOrEmpty(options.Value.Folder))
+            throw new DirectoryNotFoundException("Whisper folder not found. Please set the Whisper folder in appsettings.json");
 
         var whisperFolder = Path.GetFullPath(options.Value.Folder);
         if (Directory.Exists(whisperFolder) is false)
         {
-            Log.Warning("Whisper folder not found");
-            Log.Information("Creating Whisper folder");
+            _logger.Warning("Whisper folder not found");
+            _logger.Information("Creating Whisper folder");
             Directory.CreateDirectory(whisperFolder);
-            Log.Information("Whisper folder created");
+            _logger.Information("Whisper folder created");
         }
 
         WhisperFolder = whisperFolder;
         AudioFilesFolder = Path.Combine(whisperFolder, "AudioFiles");
-        if (Directory.Exists(AudioFilesFolder) is false)
-            Directory.CreateDirectory(AudioFilesFolder);
+
+        if (Directory.Exists(AudioFilesFolder)) return;
+        _logger.Information("Creating AudioFiles folder");
+        Directory.CreateDirectory(AudioFilesFolder);
     }
 }
