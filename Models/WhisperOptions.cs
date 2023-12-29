696afc5c064db22fcb8c573dd14b167a2fdf39a6
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-12-29 14:50:54 +0200

    doc: fix cref name

--- Models/WhisperOptions.cs
+++ Models/WhisperOptions.cs
@@ -11,6 +11,6 @@ namespace WhisperAPI.Models;
 /// <param name="Language">The language of the audio file. Can be null if the language is unknown.</param>
 /// <param name="Translate">Whether to translate the audio file to English.</param>
 /// <param name="WhisperModel">The type of GGML model to use for processing the audio file.</param>
-/// <returns>A <see cref="JsonResponse"/> object representing the response from the Whisper API.</returns>
+/// <returns>A <see cref="SegmentData"/> object representing the response from the Whisper API.</returns>
 public sealed record WhisperOptions(
     string WavFile, string? Language, bool Translate, GgmlType WhisperModel) : IRequest<List<SegmentData>>;
