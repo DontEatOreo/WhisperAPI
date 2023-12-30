201874b5d09030aafb29910ad3bbe8807d04e800
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-12-30 15:40:20 +0200

    chore(Program.cs): primary constructor

--- Program.cs
+++ Program.cs
@@ -109,19 +109,12 @@ internal static class ResultsExtensions
 }
 
 // Define a custom IResult implementation for returning HTML responses
-internal class HtmlResult : IResult
+internal class HtmlResult(string html) : IResult
 {
-    private readonly string _html;
-
-    public HtmlResult(string html)
-    {
-        _html = html;
-    }
-
     public Task ExecuteAsync(HttpContext httpContext)
     {
         httpContext.Response.ContentType = MediaTypeNames.Text.Html;
-        httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
-        return httpContext.Response.WriteAsync(_html);
+        httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
+        return httpContext.Response.WriteAsync(html);
     }
 }
